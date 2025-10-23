2f4882ae548f7c1a9fb4fc7051a55cbcda67c1f3
Kanishakgoel <128068225+Kanishakgoel@users.noreply.github.com>
2025-10-23 10:51:33 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Kanishak Geol",
+    author_email="goelkanishak423@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
